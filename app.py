10f65373c78cf33864a9b74ec288cc5eff1f3fe9
shamsherdhaka <shamsher@gmail.com>
2021-12-05 14:15:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import request
+import requests
 import pandas as pd
 
 # http://127.0.01:5000/ is from the flask api
-response = request.get("http://127.0.01:4000/")
+response = requests.get("http://127.0.01:4000/")
 print(response.json())
 data_table1 = pd.DataFrame(response.json())
 st.write(data_table1)
