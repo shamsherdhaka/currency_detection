197374b308e00763578128dbc8cbe0542467e0c3
shamsherdhaka <shamsher@gmail.com>
2021-12-05 14:17:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 
 # http://127.0.01:5000/ is from the flask api
-response = requests.get("http://127.0.01:4000/")
+response = requests.get("http://0.0.0.0:4000/")
 print(response.json())
 data_table1 = pd.DataFrame(response.json())
 st.write(data_table1)
